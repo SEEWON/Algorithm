21971616f7df6210c8f39c92fed29f33e80f2899
SEEWON <andyaaa78@gmail.com>
2022-05-08 01:08:35 +0900

    20220508

--- BOJ/11000.cpp
+++ BOJ/11000.cpp
@@ -1,39 +1,31 @@
 #include<iostream>
 #include<vector>
+#include<queue>
 #include<algorithm>
 using namespace std;
 
-int main(void) {
+int main() {
   ios::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+  cin.tie(0); cout.tie(0);
 
-  int n, p, q;
-  vector<pair<int,int>> v;
+  int n;
+  long long p, q;
+
   cin >> n;
+  vector<pair<long long ,long long>> v;
   for(int i=0;i<n;i++) {
     cin >> p >> q;
-    v.push_back(make_pair(q,p));
+    v.push_back(make_pair(p, q));
   }
   sort(v.begin(),v.end());
 
-  int classCnt=0;
-  int nowtime = v[0].first;
-  v.erase(v.begin());
-  while (v.size()!=0) {
-    auto it = v.begin();
-    while(it!=v.end()) {
-      if((*it).second>=nowtime) {
-        nowtime = (*it).first;
-        v.erase(it);
-      } else {
-        it++;
-      };
-    }
-    nowtime=0;
-    classCnt++;
+  priority_queue<long long, vector<long long>, greater<>> pq;
+  pq.push(v[0].second);
+  for(int i=1;i<n;i++) {
+    pq.push(v[i].second);
+    if(v[i].first>=pq.top()) pq.pop();
   }
+  cout << pq.size() << endl;
 
-  cout << classCnt << endl;
   return 0;
 }
